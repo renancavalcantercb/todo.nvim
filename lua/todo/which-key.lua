a47fb5d347d4c99d60512318edee5918a416f6d8
Renan Cavalcante <renancavalcantercb@protonmail.com>
2025-04-28 23:08:59 -0300

    fix: revert which-key to stable table format

--- lua/todo/which-key.lua
+++ lua/todo/which-key.lua
@@ -4,21 +4,24 @@ function M.setup()
     local which_key = require("which-key")
     
     which_key.register({
-        { "", "<leader>[D", desc = "Remove deadline", cmd = "lua require('todo').remove_deadline()<CR>" },
-        { "", "<leader>[c", desc = "Count tasks", cmd = "lua require('todo').count_tasks()<CR>" },
-        { "", "<leader>[]", desc = "Edit description", cmd = "lua require('todo').edit_task_description()<CR>" },
-        { "", "<leader>[t", desc = "Toggle task state", cmd = "lua require('todo').toggle_task_state()<CR>" },
-        { "", "<leader>[f", desc = "Toggle filter", cmd = "lua require('todo').toggle_filter()<CR>" },
-        { "", "<leader>[s", desc = "Add subtask", cmd = "lua require('todo').add_subtask()<CR>" },
-        { "", "<leader>[o", desc = "Sort by status", cmd = "lua require('todo').sort_tasks()<CR>" },
-        { "", "<leader>[-", desc = "Mark as in progress", cmd = "lua require('todo').mark_in_progress()<CR>" },
-        { "", "<leader>[[", desc = "Add new task", cmd = "lua require('todo').add_todo_item()<CR>" },
-        { "", "<leader>[l", desc = "List overdue tasks", cmd = "lua require('todo').check_overdue_tasks()<CR>" },
-        { "", "<leader>[x", desc = "Mark as completed", cmd = "lua require('todo').check_task()<CR>" },
-        { "", "<leader>[O", desc = "Sort by deadline", cmd = "lua require('todo').sort_by_deadline()<CR>" },
-        { "", "<leader>[d", desc = "Add deadline", cmd = "lua require('todo').add_deadline()<CR>" },
-        { "", group = "todo" },
+        ["["] = {
+            name = "todo",
+            ["["] = { "<cmd>lua require('todo').add_todo_item()<CR>", "Add new task" },
+            ["x"] = { "<cmd>lua require('todo').check_task()<CR>", "Mark as completed" },
+            ["-"] = { "<cmd>lua require('todo').mark_in_progress()<CR>", "Mark as in progress" },
+            ["]"] = { "<cmd>lua require('todo').edit_task_description()<CR>", "Edit description" },
+            ["c"] = { "<cmd>lua require('todo').count_tasks()<CR>", "Count tasks" },
+            ["t"] = { "<cmd>lua require('todo').toggle_task_state()<CR>", "Toggle task state" },
+            ["s"] = { "<cmd>lua require('todo').add_subtask()<CR>", "Add subtask" },
+            ["f"] = { "<cmd>lua require('todo').toggle_filter()<CR>", "Toggle filter" },
+            ["o"] = { "<cmd>lua require('todo').sort_tasks()<CR>", "Sort by status" },
+            ["d"] = { "<cmd>lua require('todo').add_deadline()<CR>", "Add deadline" },
+            ["D"] = { "<cmd>lua require('todo').remove_deadline()<CR>", "Remove deadline" },
+            ["l"] = { "<cmd>lua require('todo').check_overdue_tasks()<CR>", "List overdue tasks" },
+            ["O"] = { "<cmd>lua require('todo').sort_by_deadline()<CR>", "Sort by deadline" },
+        },
     }, {
+        prefix = "<leader>",
         mode = "n",
     })
 end
